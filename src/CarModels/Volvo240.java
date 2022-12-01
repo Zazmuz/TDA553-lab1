88597031d6026c34e5e8f1731af81babbc3b26b3
Christian <chrille_0313@hotmail.com>
2022-12-01 15:30:59 +0100

    Fixed a trimFactor bug in Volvo240
    We were not using trimFactor in the constructor

--- src/CarModels/Volvo240.java
+++ src/CarModels/Volvo240.java
@@ -9,7 +9,7 @@ import src.Car.TrimmedCar;
 public class Volvo240 extends TrimmedCar {
 
     public Volvo240(double trimFactor) {
-        super("Volvo240", Color.black, 4, 100, 5, 1.3);
+        super("Volvo240", Color.black, 4, 100, 5, trimFactor);
     }
 
     @Override

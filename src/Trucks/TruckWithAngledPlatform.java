09b08d3695988d678f1caa7e79d86ba9dcd50151
Christian <chrille_0313@hotmail.com>
2022-12-03 17:43:39 +0100

    Create TruckWithAngledPlatform.java
    Co-Authored-By: Matistjati <38278628+Matistjati@users.noreply.github.com>
    Co-Authored-By: Zazmuz <44049876+Zazmuz@users.noreply.github.com>

---
+++ src/Trucks/TruckWithAngledPlatform.java
@@ -0,0 +1,30 @@
+package src.Trucks;
+
+import src.Vehicle;
+import src.VehicleMods.AngledPlatformMod;
+
+import java.awt.*;
+
+public abstract class TruckWithAngledPlatform extends Vehicle {
+
+    private final AngledPlatformMod platformMod;
+
+    public TruckWithAngledPlatform(String modelName, Color color, int nrDoors, double enginePower, double turningRate, double maxAngle) {
+        super(modelName, color, nrDoors, enginePower, turningRate);
+
+        platformMod = new AngledPlatformMod(maxAngle) {
+            @Override
+            protected boolean canBeMoved() {
+                return getCurrentSpeed() == 0;
+            }
+        };
+    }
+
+    protected AngledPlatformMod getPlatformMod() {
+        return platformMod;
+    }
+
+    protected boolean canDrive() {
+        return !getPlatformMod().isElevated();
+    }
+}

47931b8ebe2c624489645accef0a52784a3164eb
Christian <chrille_0313@hotmail.com>
2022-12-03 18:17:55 +0100

    Refactored constructors to be more DRY

--- src/CarModels/TestCar.java
+++ src/CarModels/TestCar.java
@@ -6,16 +6,16 @@ import java.awt.*;
 
 public class TestCar extends Vehicle {
 
-    public TestCar() {
-        super("test", Color.red, 2, 125, 5);
-    }
-
     public TestCar(String name, Color color, int nrDoors, double enginePower, double turningRate) {
         super(name, color, nrDoors, enginePower, turningRate);
     }
 
     public TestCar(double enginePower) {
-        super("test", Color.red, 2, enginePower, 5);
+        this("Test", Color.red, 2, enginePower, 5);
+    }
+
+    public TestCar() {
+        this(125);
     }
 
     @Override

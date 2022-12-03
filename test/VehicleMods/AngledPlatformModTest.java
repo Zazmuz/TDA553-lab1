dacf588f659fafcbf427300d8821d778c9775a17
Christian <chrille_0313@hotmail.com>
2022-12-03 18:20:28 +0100

    Create AngledPlatformModTest.java

---
+++ test/VehicleMods/AngledPlatformModTest.java
@@ -0,0 +1,31 @@
+package test.VehicleMods;
+
+import org.junit.Test;
+import src.CarModels.Scania;
+import src.VehicleMods.AngledPlatformMod;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
+
+
+public class AngledPlatformModTest {
+
+    @Test
+    public void test_angled_platform_only_allowed_angles() {
+        AngledPlatformMod platformMod = new AngledPlatformMod(90) {
+            @Override
+            protected boolean canBeMoved() {
+                return true;
+            }
+        };
+
+        platformMod.setAngle(-10);
+        assertEquals(0, platformMod.getAngle(), 0.0);
+        platformMod.setAngle(169);
+        assertEquals(90, platformMod.getAngle(), 0.0);
+        platformMod.setAngle(69);
+        assertEquals(69, platformMod.getAngle(), 0.0);
+    }
+
+}

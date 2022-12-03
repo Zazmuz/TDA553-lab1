bb807cd69f8551b0110a485f3c111448eea9eb8f
Christian <chrille_0313@hotmail.com>
2022-12-03 22:25:16 +0100

    Refactored TruckWithAngledPlatform
    Co-Authored-By: Matistjati <38278628+Matistjati@users.noreply.github.com>
    Co-Authored-By: Zazmuz <44049876+Zazmuz@users.noreply.github.com>

--- src/Trucks/TruckWithAngledPlatform.java
+++ src/Trucks/TruckWithAngledPlatform.java
@@ -5,27 +5,38 @@ import src.VehicleMods.AngledPlatformMod;
 
 import java.awt.*;
 
+
 public abstract class TruckWithAngledPlatform extends Vehicle {
 
     private final AngledPlatformMod platformMod;
 
     public TruckWithAngledPlatform(String modelName, Color color, int nrDoors, double enginePower, double turningRate, double maxAngle) {
         super(modelName, color, nrDoors, enginePower, turningRate);
-
-        platformMod = new AngledPlatformMod(maxAngle) {
-            @Override
-            protected boolean canBeMoved() {
-                return getCurrentSpeed() == 0;
-            }
-        };
-    }
-
-    public AngledPlatformMod getPlatformMod() {
-        return platformMod;
+        platformMod = new AngledPlatformMod(maxAngle);
     }
 
     protected boolean canDrive() {
-        return !getPlatformMod().isElevated();
+        return !platformMod.isElevated();
     }
+
+    public boolean canMovePlatform() {
+        return getCurrentSpeed() == 0;
+    }
+
+    public void movePlatform(double angle) {
+        if (canMovePlatform()) {
+            platformMod.setAngle(angle);
+        }
+    }
+
+    public boolean platformIsLowered() { return platformMod.isLowered(); }
+
+    public boolean platformIsRaised() { return platformMod.isRaised(); }
+
+    public boolean platformIsElevated() { return platformMod.isElevated(); }
+
+    public double getMaxAngle() { return platformMod.getMaxAngle(); }
+
+    public double getPlatformAngle() { return platformMod.getAngle(); }
 
 }
